315b61841ccaebf6fbb179c4fc8a7108953cb095
Pavel Skrytski <pavelskrytski@gmail.com>
2020-02-15 07:00:48 +0300

    add google calendar chart

--- src/components/YearChart.js
+++ src/components/YearChart.js
@@ -6,6 +6,41 @@ import 'react-calendar-heatmap/dist/styles.css';
 
 function YearChart() {
   const [items, setItems] = useState([]);
+
+  const options = {
+    title: "Age vs. Weight comparison",
+    legend: "none"
+  };
+
+  const data = [
+    [{ type: 'date', id: 'Date' }, { type: 'number', id: 'Won/Loss' }],
+    [new Date(2013, 2, 4), 10],
+    [new Date(2013, 2, 5), 3],
+    [new Date(2013, 2, 7), -1],
+    [new Date(2013, 2, 8), 2],
+    [new Date(2013, 2, 12), -1],
+    [new Date(2013, 2, 13), 1],
+    [new Date(2013, 2, 15), 1],
+    [new Date(2013, 2, 16), -4],
+    [new Date(2013, 1, 4), 10],
+    [new Date(2013, 1, 5), 3],
+    [new Date(2013, 1, 7), -1],
+    [new Date(2013, 1, 8), 2],
+    [new Date(2013, 1, 12), -1],
+    [new Date(2013, 1, 13), 1],
+    [new Date(2013, 1, 15), 1],
+    [new Date(2013, 1, 16), -4],
+  ];
+
+  const dataNew = (items) => {
+    let newDate = []
+    let type = [{ type: 'date', id: 'Date' }, { type: 'number', id: 'Won/Loss' }]
+    newDate.push(type)
+    items.map(item => newDate.push([new Date(item.date), item.count]))
+    return newDate
+  }
+
+
   useEffect(() => {
     fetch("http://127.0.0.1:5000/api/v1/year-sales")
       .then(res => res.json())
@@ -18,7 +53,7 @@ function YearChart() {
         <CalendarHeatmap
           startDate={new Date('2019-01-01')}
           endDate={new Date('2019-12-31')}
-          gutterSize={0}
+          gutterSize={0.5}
           values={items.map((item) => {
             return {
               'date': item.date,
@@ -31,6 +66,19 @@ function YearChart() {
             return `color-scale-${value.count}`;
           }}
         />
+      </div>
+      <div>
+        <div className={"my-pretty-chart-container"}>
+          <Chart
+            chartType="Calendar"
+            data={dataNew(items)}
+            options={options}
+            width={1000}
+            height="400px"
+            legendToggle
+            rootProps={{ 'data-testid': '2' }}
+          />
+        </div>
       </div>
     </div>
   );
